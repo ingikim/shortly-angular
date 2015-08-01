a8ae67d8f445bbac19b0a206c07479e3c609d603
ingikim <bcmet365@daum.net>
2015-07-31 17:30:24 -0700

    Got through about half of the basic requirements

--- client/app/app.js
+++ client/app/app.js
@@ -15,8 +15,20 @@ angular.module('shortly', [
       templateUrl: 'app/auth/signup.html',
       controller: 'AuthController'
     })
+    .when('/links', {
+      templateUrl: 'app/links/links.html',
+      controller: 'LinksController'
+    })
+    .when('/shorten', {
+      templateUrl: 'app/shorten/shorten.html',
+      controller: 'ShortenController'
+    })
+    .when('/', {
+    templateUrl: 'app/auth/signin.html',
+      controller: 'AuthController'  
+    })
     // Your code here
-
+    console.log('Ran route');
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
     $httpProvider.interceptors.push('AttachTokens');
--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,25 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
-  // Your code here
+.controller('LinksController', function ($scope, Links, $location) {
+  $scope.data = {};
+
+  $scope.getLinks = function () {
+    Links.getLinks()
+    .then(function(data){
+      $scope.data = data;
+    });
+  };
+
+  $scope.indexClick = function () {
+    $location.path('/links');
+  };
+
+  $scope.createClick = function () {
+    $location.path('/shorten');
+  };
+
+
+  $scope.followLink = function(link){};
+  $scope.getLinks();
+
 });
